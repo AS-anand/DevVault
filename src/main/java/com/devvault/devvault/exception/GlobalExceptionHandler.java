b68dba447bda98f03e2290a9a1f257fce9758a70
Anurag Anand <anurag.anand.2pb@gmail.com>
2026-08-30 19:43:45 +0530

    Implement Auth Feature

---
+++ src/main/java/com/devvault/devvault/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.devvault.devvault.exception;
+
+import com.devvault.devvault.auth.exception.EmailAlreadyExistsException;
+import com.devvault.devvault.auth.exception.InvalidCredentialsException;
+import com.devvault.devvault.auth.exception.UsernameAlreadyExistsException;
+import com.devvault.devvault.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(
+            EmailAlreadyExistsException exception
+    ) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                exception.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameAlreadyExists(
+            UsernameAlreadyExistsException exception
+    ) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                exception.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials(
+            InvalidCredentialsException exception
+    ) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                exception.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(response);
+    }
+}
---
+++ src/main/java/com/devvault/devvault/exception/dto/SuccessResponse.java
@@ -0,0 +1,20 @@
+package com.devvault.devvault.exception.dto;
+
+public class SuccessResponse {
+
+    private int status;
+    private String message;
+
+    public SuccessResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
